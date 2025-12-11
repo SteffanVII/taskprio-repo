0ff1a48844621e6c93292c6f3e47ad77ecf1fcd2
JayAbao <88617674+SteffanVII@users.noreply.github.com>
2025-12-11 18:48:45 +0800

    Update authentication.ts

--- apps/taskprio-api/src/routes/authentication/authentication.ts
+++ apps/taskprio-api/src/routes/authentication/authentication.ts
@@ -18,8 +18,6 @@ function reigsterAuthenticationRoutes() {
         `/redirect-to-electron-app`,
         async ( req : Request, res : Response ) => {
             const { code, state } = req.query;
-            const query = req.query;
-            console.log( query )
             console.log( req.url )
             console.log( req.originalUrl )
             console.log( code, state )
