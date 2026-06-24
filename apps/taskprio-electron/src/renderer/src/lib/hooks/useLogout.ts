d23faabd15737dd0ba3126691dce770dfc3dc364
JayAbao <88617674+SteffanVII@users.noreply.github.com>
2026-06-24 19:37:09 +0800

    Updated aws-sdk and google-auth-library

--- apps/taskprio-electron/src/renderer/src/lib/hooks/useLogout.ts
+++ apps/taskprio-electron/src/renderer/src/lib/hooks/useLogout.ts
@@ -2,17 +2,17 @@ import { EWebsocketConnectionState, WebSocketContext } from "@/components/others
 import { useLogoutRequest } from "@/services/authentication"
 import { useQueryClient } from "@tanstack/react-query"
 import Cookies from "js-cookie"
-import { useContext, useLayoutEffect } from "react"
-import { useNavigate } from "@tanstack/react-router"
+import { useContext } from "react"
 import { useGlobalsStore } from "@/stores/globals"
-import { useTaskboardDragStore, useTaskboardDragStore_taskboardSectionDrag } from "@/stores/taskboardDrag"
+import { useTaskboardDragStore } from "@/stores/taskboardDrag"
 import { useSessionHistoryTabStore } from "@/stores/sessionHistoryTab"
 import { useTaskTodoPageStore } from "@/stores/taskTodoPage"
 import { useDialogsStore } from "@/stores/dialogs"
 import { useWorkspaceStore } from "@/stores/workspace"
 import { useProjectStore } from "@/stores/project"
 import { useTaskboardStore } from "@/stores/taskboard"
-import { AUTH_TOKEN_KEY } from "../globals"
+import { AUTH_TOKEN_KEY, USER_DATA_KEY } from "../globals"
+import { useNavigate } from "@tanstack/react-router"
 
 export type TUseLogout = {
   logout: () => Promise<void>
@@ -23,6 +23,7 @@ export type TUseLogout = {
 export const useLogout = (): TUseLogout => {
 
   const queryClient = useQueryClient()
+  const navigate = useNavigate()
 
   const resetGlobalsStore = useGlobalsStore(state => state.resetStore)
   const resetTaskboardDragStore = useTaskboardDragStore(state => state.resetStore)
@@ -47,10 +48,13 @@ export const useLogout = (): TUseLogout => {
       if (connectionState === EWebsocketConnectionState.OPEN) closeWebSocketConnection()
       Cookies.remove("access_token")
       localStorage.removeItem(AUTH_TOKEN_KEY)
+      localStorage.removeItem(USER_DATA_KEY)
       localStorage.removeItem(import.meta.env.VITE_LAST_WORKSPACE_VISTED_COOKIE_NAME)
       localStorage.removeItem(import.meta.env.VITE_LAST_PROJECT_VISITED_COOKIE_NAME)
       localStorage.removeItem(import.meta.env.VITE_IGNORE_TODO_SESSION_IS_ACTIVE_WARNING_LOCAL_STORAGE_NAME)
-      // navigate("/login")
+      navigate({
+        to: "/login"
+      })
       setTimeout(() => {
         resetGlobalsStore()
         resetTaskboardDragStore()
